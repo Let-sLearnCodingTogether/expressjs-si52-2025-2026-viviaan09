b07ed3105ec2cb3288c90073f539e399aa81daa2
viviaan09 <ngvivian682@gmail.com>
2025-09-24 20:40:17 +0700

    update public-profile.ejs from style.css & set route

--- routes/web.js
+++ routes/web.js
@@ -6,7 +6,12 @@ web.get('/',(req,res) =>{
     res.render('index')
 } )
 web.get('/:username', (req,res)=>{
-    res.render('public-profile')
+    const username = req.params.username;
+    res.render('public-profile',{
+        title: username,
+        username: username,
+        bio: "test 1234"
+    })
 })
 
 export default web 
